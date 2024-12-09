6478665508c112dfe2f1e071fbe2952b2a41cb6a
DuanC001 <370966257@qq.com>
2024-12-08 16:52:47 -0800

    commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
+    proxy: {
+      "/api/v1": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
+});
